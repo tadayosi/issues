aa582bcecf539f276791b3ed00b41af7fb257406
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2016-06-17 14:26:22 +0900

    Added another test

--- ENTESB-5661/camel-charset/src/test/java/com/redhat/issues/camel/charset/CharsetTest.java
+++ ENTESB-5661/camel-charset/src/test/java/com/redhat/issues/camel/charset/CharsetTest.java
@@ -4,8 +4,8 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class CharsetTest extends CamelTestSupport {
 
@@ -34,6 +34,20 @@ public class CharsetTest extends CamelTestSupport {
         //template.sendBody("direct:input", "あいうえお".getBytes("UTF-8"));
         //template.sendBody("direct:input", "あいうえお".getBytes("ISO-8859-1"));
         assertMockEndpointsSatisfied();
+    }
+
+    @Test
+    public void javaString() throws Exception {
+        String s1 = "éabcd";
+        String s2 = "あいうえお";
+
+        assertThat(new String(s1.getBytes("UTF-8"), "UTF-8"), is(s1));
+        assertThat(new String(s1.getBytes("ISO-8859-1"), "ISO-8859-1"), is(s1));
+        //assertThat(new String(s1.getBytes("ISO-8859-1"), "UTF-8"), is(s1)); // Fail!
+
+        assertThat(new String(s2.getBytes("UTF-8"), "UTF-8"), is(s2));
+        String garbled = new String(s2.getBytes("UTF-8"), "ISO-8859-1");
+        assertThat(new String(garbled.getBytes("ISO-8859-1"), "UTF-8"), is(s2));
     }
 
 }
