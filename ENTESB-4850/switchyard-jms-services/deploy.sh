202071eeae461c22630abb94ecefb696d778f586
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2016-08-22 17:27:28 +0900

    Added clean up command to deploy.sh

--- ENTESB-4850/switchyard-jms-services/deploy.sh
+++ ENTESB-4850/switchyard-jms-services/deploy.sh
@@ -5,4 +5,10 @@ if [ -z $EAP_HOME ]; then
     exit
 fi
 
-cp -v jms*/target/switchyard-jms-services-jms*.jar $EAP_HOME/standalone/deployments/
+if [ "$1" = "clean" ]; then
+    echo "Cleaning up $EAP_HOME/standalone/deployments/"
+    rm -v $EAP_HOME/standalone/deployments/switchyard-jms-services-jms*.jar*
+else
+    echo "Deploying to $EAP_HOME/standalone/deployments/"
+    cp -v jms*/target/switchyard-jms-services-jms*.jar $EAP_HOME/standalone/deployments/
+fi
