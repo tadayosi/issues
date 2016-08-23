7de37cec18535aadf4eda38d685fe2a50bde3f09
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2016-08-23 16:52:32 +0900

    Improved reproducer

---
+++ ENTESB-4850/switchyard-jms-services/jms9/src/main/java/com/redhat/issues/switchyard/jms9/PoolingConnectionFactory.java
@@ -0,0 +1,39 @@
+package com.redhat.issues.switchyard.jms9;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import javax.jms.ConnectionFactory;
+import javax.naming.InitialContext;
+
+public class PoolingConnectionFactory {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PoolingConnectionFactory.class);
+
+    @Produces
+    @Named("PoolingConnectionFactory")
+    public ConnectionFactory create() {
+        // Try 10 times
+        for (int i = 0; i < 60; i++) {
+            InitialContext context = null;
+            try {
+                context = new InitialContext();
+                return (ConnectionFactory) context.lookup("ConnectionFactory");
+            } catch (Exception e) {
+                LOG.warn("##### {} - Failed to lookup ConnectionFactory. Retrying #####", i);
+            } finally {
+                if (context != null) {
+                    try {
+                        context.close();
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+}
