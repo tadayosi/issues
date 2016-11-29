170742b1428fd99afa2c893f472d1570cb3c1fcc
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2016-11-29 15:40:53 +0900

    Reproducer

---
+++ ENTESB-6281/wildfly-jmx-auth/src/main/java/com/redhat/issues/wildfly/JmxServlet.java
@@ -0,0 +1,94 @@
+package com.redhat.issues.wildfly;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.management.ManagementFactory;
+import java.security.PrivilegedAction;
+import java.util.Arrays;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import javax.security.auth.Subject;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet("/jmx")
+public class JmxServlet extends HttpServlet {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JmxServlet.class);
+
+    private static final String DOMAIN = "hawtio-domain";
+    private static final String USERNAME = "hawtio";
+    private static final String PASSWORD = "p@ssw0rd";
+
+    private MBeanServer mBeanServer;
+
+    @Override
+    public void init() {
+        mBeanServer = ManagementFactory.getPlatformMBeanServer();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.info("GET: {}", request.getRequestURI());
+        process(request, response);
+    }
+
+    private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Subject subject = login();
+        LOG.info("Subject: {}", subject);
+
+        StringBuilder value = new StringBuilder();
+        Subject.doAs(subject, (PrivilegedAction<Object>) () -> {
+            value.append(readMBeanAttribute(request));
+            return null;
+        });
+        LOG.info("value = {}", value);
+
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.print(value);
+        out.close();
+    }
+
+    private Subject login() {
+        Subject subject = new Subject();
+        try {
+            new LoginContext(DOMAIN, subject, cs -> {
+                Arrays.stream(cs).forEach(c -> {
+                    if (c instanceof NameCallback) {
+                        ((NameCallback) c).setName(USERNAME);
+                    } else if (c instanceof PasswordCallback) {
+                        ((PasswordCallback) c).setPassword(PASSWORD.toCharArray());
+                    } else {
+                        LOG.debug("Unknown callback class: {}", c.getClass().getName());
+                    }
+                });
+            }).login();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+        return subject;
+    }
+
+    private Object readMBeanAttribute(HttpServletRequest request) {
+        try {
+            return mBeanServer.getAttribute(
+                new ObjectName(request.getParameter("name")),
+                request.getParameter("attribute"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+}
